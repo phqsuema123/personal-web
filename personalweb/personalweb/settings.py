07474170fbc77152b2218ff64700e64170a5391e
Phadlaan <111688941+phqsuema123@users.noreply.github.com>
2024-07-31 13:21:34 +0700

    updatecss

--- personalweb/personalweb/settings.py
+++ personalweb/personalweb/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'personalweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR,'C:/Users/ASUS/Desktop/web-j-anas/personal-web/personalweb/myweb/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+import os
+
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
