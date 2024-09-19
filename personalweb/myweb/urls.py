81fed60e59778b0e033927bea0ee46fc38254fe4
Phadlaan <111688941+phqsuema123@users.noreply.github.com>
2024-09-19 13:40:08 +0700

    add

--- personalweb/myweb/urls.py
+++ personalweb/myweb/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from myweb import views
 urlpatterns =[
-    path('',views.index)
+    path('',views.index),
+    path('image_activity',views.image_activity)
 ]
--- personalweb/myweb/views.py
+++ personalweb/myweb/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def index(request):
     return render(request,"index.html")
+
+def image_activity(request):
+    return render(request,"image_activity.html")
+
